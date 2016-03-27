90693aefd9dac8b20394c02c9a86ed8ac13f3db5
yelouafi <yelouafi@gmail.com>
2016-03-27 18:32:07 +0100

    support take(observable, pattern)

--- test/proc/take.js
+++ test/proc/take.js
@@ -1,24 +1,19 @@
 import test from 'tape';
 import proc from '../../src/internal/proc'
+import emitter from '../../src/internal/emitter'
 import * as io from '../../src/effects'
 
-const DELAY = 50
-const delay = (ms) => () => new Promise(resolve => setTimeout(resolve, ms))
 
-test('processor take handling', assert => {
+test('processor take from default observable', assert => {
   assert.plan(1);
 
   let actual = [];
   const input = (cb) => {
     Promise.resolve(1)
       .then(() => cb({type: 'action-*'}))
-      .then(delay(0))
       .then(() => cb({type: 'action-1'}))
-      .then(delay(0))
       .then(() => cb({type: 'action-2'}))
-      .then(delay(0))
       .then(() => cb({isAction: true}))
-      .then(delay(0))
       .then(() => cb({type: 'action-3'}))
     return () => {}
   }
@@ -26,7 +21,7 @@ test('processor take handling', assert => {
   function* genFn() {
     actual.push( yield io.take('action-*') )
     actual.push( yield io.take('action-1') )
-    actual.push( yield io.take('action-2', 'action-2222') )
+    actual.push( yield io.take(['action-2', 'action-2222']) )
     actual.push( yield io.take(a => a.isAction) )
     actual.push( yield io.take('action-2222') )
   }
@@ -37,9 +32,47 @@ test('processor take handling', assert => {
 
   setTimeout(() => {
     assert.deepEqual(actual, expected,
-      "processor must fullfill input queries from the generator"
+      "processor must fullfill take Effects from default observable"
     );
     assert.end();
-  }, DELAY)
+  }, 0)
+
+});
+
+
+test('processor take from provided observable', assert => {
+  assert.plan(1);
+
+  const observable = emitter()
+  let actual = [];
+  const end = {}
+
+  Promise.resolve(1)
+    .then(() => observable.emit({type: 'action-*'}))
+    .then(() => observable.emit({type: 'action-1'}))
+    .then(() => observable.emit({type: 'action-2'}))
+    .then(() => observable.emit({isAction: true}))
+    .then(() => observable.emit({type: 'action-3'}))
+    .then(() => observable.emit(end))
+
+
+  function* genFn() {
+    actual.push( yield io.take(observable, 'action-*') )
+    actual.push( yield io.take(observable, 'action-1') )
+    actual.push( yield io.take(observable, ['action-2', 'action-2222']) )
+    actual.push( yield io.take(observable, a => a.isAction) )
+    actual.push( yield io.take(observable, 'action-2222') )
+  }
+
+  proc(genFn()).done.catch(err => assert.fail(err))
+
+  const expected = [{type: 'action-*'}, {type: 'action-1'}, {type: 'action-2'}, {isAction: true}];
+
+  setTimeout(() => {
+    assert.deepEqual(actual, expected,
+      "processor must fullfill take Effects from a provided observable"
+    );
+    assert.end();
+  }, 0)
 
 });
